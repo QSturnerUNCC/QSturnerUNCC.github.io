f904cf20f69f383d1e5168f35424e66cc2a43be1
QSturner <58527976+QSturnerUNCC@users.noreply.github.com>
2021-11-21 19:06:05 -0500

    Final Activity 14 Commit

--- itis3135/activities/scripts/Sturner-Activity14.js
+++ itis3135/activities/scripts/Sturner-Activity14.js
@@ -4,14 +4,76 @@ $(document).ready(function() {
         $("#month").html("");
         $("#speaker").html("");
         $("#text").html("");
+        $("#image").html("");
 
         $.getJSON("toobin.json", function(data) {
             $.each(data, function() {
                 $.each(this, function(key, value) {
                     $("#title").append(value.title);
-                    $("#month").append(value.image);
+                    $("#month").append(value.month);
                     $("#speaker").append(value.speaker);
                     $("#text").append(value.text);
+                    $("#image").attr("src", value.image)
+                });
+            });
+        });
+    });
+
+    $("a[title|='sorkin']").click(function() {
+        $("#title").html("");
+        $("#month").html("");
+        $("#speaker").html("");
+        $("#text").html("");
+        $("#image").html("");
+
+        $.getJSON("sorkin.json", function(data) {
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $("#title").append(value.title);
+                    $("#month").append(value.month);
+                    $("#speaker").append(value.speaker);
+                    $("#text").append(value.text);
+                    $("#image").attr("src", value.image)
+                });
+            });
+        });
+    });
+
+    $("a[title|='chua']").click(function() {
+        $("#title").html("");
+        $("#month").html("");
+        $("#speaker").html("");
+        $("#text").html("");
+        $("#image").html("");
+
+        $.getJSON("chua.json", function(data) {
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $("#title").append(value.title);
+                    $("#month").append(value.month);
+                    $("#speaker").append(value.speaker);
+                    $("#text").append(value.text);
+                    $("#image").attr("src", value.image)
+                });
+            });
+        });
+    });
+
+    $("a[title|='sampson']").click(function() {
+        $("#title").html("");
+        $("#month").html("");
+        $("#speaker").html("");
+        $("#text").html("");
+        $("#image").html("");
+
+        $.getJSON("sampson.json", function(data) {
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $("#title").append(value.title);
+                    $("#month").append(value.month);
+                    $("#speaker").append(value.speaker);
+                    $("#text").append(value.text);
+                    $("#image").attr("src", value.image)
                 });
             });
         });
