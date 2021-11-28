b7b66e26c5689775081ef46b6b5e2fcc83816cc9
QSturner <58527976+QSturnerUNCC@users.noreply.github.com>
2021-11-28 14:30:12 -0500

    Final activity 16 commit

--- itis3135/activities/scripts/Sturner-Activity16.js
+++ itis3135/activities/scripts/Sturner-Activity16.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
 
+    $.getJSON(url, function(data) {
+        $.each(data.items, function(i, item) {
+            $("#new_building").append(
+                "<a href=" + item.media.m + " data-lightbox=\"building\"><img src=" + item.media.m + "></a>"
+            );
+        });
+    });
 });
