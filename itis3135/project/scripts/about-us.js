af68f641d4d95bdde19bf76ac4fe9f20717fe85a
QSturner <58527976+QSturnerUNCC@users.noreply.github.com>
2021-12-12 21:33:50 -0500

    Pulled data from external files using JSON, added specific product page

--- itis3135/project/scripts/about-us.js
+++ itis3135/project/scripts/about-us.js
@@ -1,5 +1,31 @@
 $(document).ready(function() 
 {
+    $.getJSON("andersen.json", function(data) 
+    {
+        $.each(data, function()
+        {
+            $.each(this, function(key, value) 
+            {
+                $("#andersen-heading").html(value.name);
+                $(".andersen-image").attr("src", value.image);
+                $("#andersen-bio").html(value.bio);
+            });
+        });
+    });
+
+    $.getJSON("shawn.json", function(data)
+    {
+        $.each(data, function()
+        {
+            $.each(this, function(key, value)
+            {
+                $("#shawn-heading").html(value.name);
+                $(".shawn-image").attr("src", value.image);
+                $("#shawn-bio").html(value.bio);
+            });
+        });
+    });
+
     $(".andersen-picture-container").tooltip();
 
     $(".shawn-picture-container").tooltip();
